06948f5e3e62d46c7ed58a40694d0069d3b47891
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-02-20 21:36:44 +0100

    Deleted unnecessary import

--- gym_pybullet_drones/examples/learn.py
+++ gym_pybullet_drones/examples/learn.py
@@ -18,7 +18,6 @@ import torch
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnMaxEpisodes
-from stable_baselines3.common.evaluation import evaluate_policy
 
 from gym_pybullet_drones.utils.Logger import Logger
 from gym_pybullet_drones.envs.HoverAviary import HoverAviary
