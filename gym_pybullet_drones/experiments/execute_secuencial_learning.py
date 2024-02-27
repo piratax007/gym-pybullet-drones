0945221d18d0a6721553265992e8ba76aac101d2
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-02-27 22:33:32 +0100

    Added execution for two different environments

--- gym_pybullet_drones/experiments/execute_secuencial_learning.py
+++ gym_pybullet_drones/experiments/execute_secuencial_learning.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
-from gym_pybullet_drones.envs import SimBasicReward
+from gym_pybullet_drones.envs import SimBasicReward, RewardRPMsAndC9
 from gym_pybullet_drones.experiments.learning_script import run_learning
 
+print("######### Learning with RewardRPMsAndC9 Environment #########")
+run_learning(env_name=RewardRPMsAndC9,
+             num_episodes=int(1e6),
+             )
+
+print("######### Learning with SimBasicReward Environment #########")
 run_learning(env_name=SimBasicReward,
-             num_episodes=int(1e3),
+             num_episodes=int(1e6),
              )
