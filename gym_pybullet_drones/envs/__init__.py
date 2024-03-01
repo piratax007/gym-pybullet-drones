dd5d95f9f44518e0b06b2642a77cf388ce949142
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-03-01 15:16:00 +0100

    Added three new environments with different reward function for continuous learning

--- gym_pybullet_drones/envs/__init__.py
+++ gym_pybullet_drones/envs/__init__.py
@@ -2,6 +2,9 @@ from gym_pybullet_drones.envs.BetaAviary import BetaAviary
 from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
 from gym_pybullet_drones.envs.HoverAviary import HoverAviary
 from gym_pybullet_drones.envs.SimBasicReward import SimBasicReward
+from gym_pybullet_drones.envs.BasicRewardWithR import BasicRewardWithR
+from gym_pybullet_drones.envs.BasicRewardWithRAndWe import BasicRewardWithRAndWe
+from gym_pybullet_drones.envs.BasicRewardWithRWeAndBi import BasicRewardWithRWeAndBi
 from gym_pybullet_drones.envs.EnvironmentTest import EnvironmentTest
 from gym_pybullet_drones.envs.MultiHoverAviary import MultiHoverAviary
 from gym_pybullet_drones.envs.VelocityAviary import VelocityAviary
--- gym_pybullet_drones/experiments/execute_secuencial_learning.py
+++ gym_pybullet_drones/experiments/execute_secuencial_learning.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python3
 
-from gym_pybullet_drones.envs import SimBasicReward
+from gym_pybullet_drones.envs import BasicRewardWithR, BasicRewardWithRAndWe, BasicRewardWithRWeAndBi
 from gym_pybullet_drones.experiments.learning_script import run_learning
 
-print("######### Learning with SimBasicReward Environment #########")
-# R(t) = 25 - 15te - 100Bo
-# Target = [0 0 1 0 0 1.7]
-run_learning(env_name=SimBasicReward,
-             learning_id="SimBasicReward",
-             num_episodes=int(5e6),
+print("""######### Learning with BasicRewardWithR Environment #########
+R(t) = 25 - 15te - 100Bo - 5R
+Target = [0 0 1 0 0 1.7]
+Time steps = 10e6
+Episode Length = 15s""")
+
+run_learning(env_name=BasicRewardWithR,
+             learning_id="Continuos-BasicRewardWithR",
+             continuous_learning=True
+             )
+
+print("""######### Learning with BasicRewardWithR Environment #########
+R(t) = 25 - 15te - 100Bo - 5R - 10we
+Target = [0 0 1 0 0 1.7]
+Time steps = 10e6
+Episode Length = 15s""")
+
+run_learning(env_name=BasicRewardWithRAndWe,
+             learning_id="Continuos-BasicRewardWithRAndWe",
+             continuous_learning=True
+             )
+
+print("""######### Learning with BasicRewardWithR Environment #########
+R(t) = 25 - 15te - 100Bo - 5R - 10we + 100Bi
+Target = [0 0 1 0 0 1.7]
+Time steps = 10e6
+Episode Length = 15s""")
+
+run_learning(env_name=BasicRewardWithRWeAndBi,
+             learning_id="Continuos-BasicRewardWithRWeAndBi",
+             continuous_learning=True
              )
