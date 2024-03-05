90be1d4bf55f43232edade6d0e2e1ef152946df0
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-03-05 22:42:22 +0100

    Added three new learning process two from scratch one using the best old trained

--- gym_pybullet_drones/experiments/execute_secuencial_learning.py
+++ gym_pybullet_drones/experiments/execute_secuencial_learning.py
@@ -5,13 +5,50 @@ from gym_pybullet_drones.experiments.learning_script import run_learning
 print("""######### Learning with Basic-Reward-With-Pitch-Roll-Penalty Environment #########
 R(t) = 25 - 15te - 100Bo + 15Per
 Target = [0 0 1 0 0 1.7]
-Time steps = 10e6
-Episode Length = 15s""")
+Episodes = 1 Million
+Episode Length = 8s""")
 
 results = run_learning(environment=BasicRewardWithPitchRollPenalty,
-                       learning_id="Test",
+                       learning_id="from-scratch-1M-episodes",
+                       episodes=1e6,
+                       parallel_environments=10
+                       )
+
+print(f"""
+################# Learning End ########################
+Results: {results}
+#######################################################
+""")
+
+print("""######### Learning with Basic-Reward-With-Pitch-Roll-Penalty Environment #########
+R(t) = 25 - 15te - 100Bo + 15Per
+Target = [0 0 1 0 0 1.7]
+Episodes = 2.5 Million
+Episode Length = 8s""")
+
+results = run_learning(environment=BasicRewardWithPitchRollPenalty,
+                       learning_id="from-scratch-2hM-episodes",
                        episodes=2.5e6,
                        parallel_environments=10
+                       )
+
+print(f"""
+################# Learning End ########################
+Results: {results}
+#######################################################
+""")
+
+print("""######### Learning with Basic-Reward-With-Pitch-Roll-Penalty Environment #########
+R(t) = 25 - 15te - 100Bo + 15Per
+Target = [0 0 1 0 0 1.7]
+Episodes = 2.5 Million
+Episode Length = 8s""")
+
+results = run_learning(environment=BasicRewardWithPitchRollPenalty,
+                       learning_id="from-old-best",
+                       continuous_learning=True,
+                       stop_on_max_episodes=False,
+                       time_steps=2e6
                        )
 
 print(f"""
