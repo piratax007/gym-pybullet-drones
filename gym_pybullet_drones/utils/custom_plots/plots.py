4bc061c2f4175a6cd71fdb68daab5053a40ea3f4
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-03-24 21:39:19 +0100

    Extracted set of axes limits and labels to avoid DRY

--- gym_pybullet_drones/utils/custom_plots/plots.py
+++ gym_pybullet_drones/utils/custom_plots/plots.py
@@ -19,52 +19,48 @@ def get_data_from_csv(file: str) -> tuple:
             return x, y, None
 
 
-def traces_from_csv(
-        files: list,
-        axis: plt.Axes,
-        color_mode: str = 'auto',
-        color_list: list = None
-) -> None:
+def traces_from_csv(files: list, axis: plt.Axes, **colors: dict) -> None:
     for i, file in enumerate(files):
         data = get_data_from_csv(file)
-        axis.plot(*data, color_list[i] if color_mode != 'auto' else '')
+        axis.plot(*data, colors['color_list'][i] if colors['color_mode'] != 'auto' else '')
 
 
-def traces_2D(**kwargs) -> None:
-    # ToDo: DRY
+def set_axis(axis: plt.Axes, **settings: dict) -> None:
+    if settings['limits']['mode'] != 'auto':
+        axis.set_xlim(*settings['limits']['x_range'])
+        axis.set_ylim(*settings['limits']['y_range'])
+        try:
+            axis.set_zlim(*settings['limits']['z_range'])
+        except KeyError:
+            pass
+
+    axis.set_xlabel(settings['labels']['x_label'], labelpad=20)
+    axis.set_ylabel(settings['labels']['y_label'], labelpad=20)
+    try:
+        axis.set_zlabel(settings['labels']['z_label'], labelpad=20)
+    except KeyError:
+        pass
+
+    axis.set_title(settings['labels']['title'])
+
+
+def single_axis_2D(files: list, colors: dict, settings: dict) -> None:
     _, axis = plt.subplots(1)
 
-    traces_from_csv(kwargs['files'], axis)
-
-    if kwargs['axes_limits']['mode'] != 'auto':
-        axis.set_xlim(*kwargs['axes_limits']['x_range'])
-        axis.set_ylim(*kwargs['axes_limits']['y_range'])
-
-    axis.set_xlabel(kwargs['x_label'])
-    axis.set_ylabel(kwargs['y_label'])
-    axis.set_title(kwargs['title'])
+    traces_from_csv(files, axis, **colors)
+    set_axis(axis, **settings)
 
     plt.show()
 
 
-def traces_3D(**kwargs) -> None:
-    # ToDo: DRY
+def single_axis_3D(files: list, colors: dict, settings: dict) -> None:
     font = {'family': 'serif', 'weight': 'bold', 'size': 15}
     plt.rc('font', **font)
     figure = plt.figure()
     axis = figure.add_subplot(projection='3d')
 
-    traces_from_csv(kwargs['files'], axis)
-
-    if kwargs['axes_limits']['mode'] != 'auto':
-        axis.set_xlim(*kwargs['axes_limits']['x_range'])
-        axis.set_ylim(*kwargs['axes_limits']['y_range'])
-        axis.set_zlim(*kwargs['axes_limits']['z_range'])
-
-    axis.set_xlabel(kwargs['x_label'], labelpad=20)
-    axis.set_ylabel(kwargs['y_label'], labelpad=20)
-    axis.set_zlabel(kwargs['z_label'], labelpad=20)
-    axis.set_title(kwargs['title'])
+    traces_from_csv(files, axis, **colors)
+    set_axis(axis, **settings)
 
     plt.show()
 
@@ -74,14 +70,21 @@ if __name__ == '__main__':
         '../../experiments/results/save-TEST-WITH-0.0052RPMS-SQUARED-DIFFERENCE-2M-03.19.2024_10.08.48/'
     )
 
-    traces_3D(
+    single_axis_3D(
         files=['save-flight-starting-from-x0y1z0-03.19.2024_15.26.29/test_3D.csv'],
-        colors='auto',
-        x_label='x (m)',
-        y_label='y (m)',
-        z_label='z (m)',
-        axes_limits=dict(
-            mode='auto'
+        colors=dict(color_mode='auto'),
+        settings=dict(
+            limits=dict(
+                mode='custom',
+                x_range=(-1, 1),
+                y_range=(-1, 1),
+                z_range=(0, 2)
+            ),
+            labels=dict(
+                x_label='x (m)',
+                y_label='y (m)',
+                z_label='z (m)',
+                title="Trajectory From [0 1 0]"
+            )
         ),
-        title='Trajectory From [0 1 0]'
     )
