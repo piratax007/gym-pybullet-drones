90d6270bb8e422cc85579a51129b91d39e679e5c
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-03-28 00:10:03 +0100

    Changed training length and description

--- gym_pybullet_drones/experiments/execute_secuencial_learning.py
+++ gym_pybullet_drones/experiments/execute_secuencial_learning.py
@@ -5,16 +5,16 @@ from gym_pybullet_drones.experiments.learning_script import run_learning
 print("""######### Learning from third best #########
 R(t) = 25 - 20te - 100Bo + 20P - 18we
 Target = [0 0 1 0 0 np.pi/2]
-Time steps = 20M
+Time steps = 10M
 Episode Length = 8s
 ##################################################
 """)
 
 results = run_learning(environment=FromScratch,
-                       learning_id="NEW-FROM-SCRATCH",
+                       learning_id="FROM-SCRATCH-10M-EPISODES",
                        parallel_environments=4,
                        time_steps=int(10e8),
-                       episodes=int(20e6)
+                       episodes=int(10e6)
                        )
 
 print(f"""
