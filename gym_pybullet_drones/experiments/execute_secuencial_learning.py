ee8a40236c6af767c5321b6ee1237b4fea120ca9
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-03-30 00:46:15 +0100

    Added new environment for continuous learning with observation space size 12 and shrinking boundaries, and ran a new from scratch using 20 million time steps instead of episodes.

--- gym_pybullet_drones/experiments/execute_secuencial_learning.py
+++ gym_pybullet_drones/experiments/execute_secuencial_learning.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python3
-from gym_pybullet_drones.envs import FromScratch
+from gym_pybullet_drones.envs import FromScratch, FromScratchShrink
 from gym_pybullet_drones.experiments.learning_script import run_learning
 
-print("""######### Learning from third best #########
+print("""######### Learning from best from scratch #########
 R(t) = 25 - 20te - 100Bo + 20P - 18we
 Target = [0 0 1 0 0 np.pi/2]
-Time steps = 10M
-Episode Length = 8s
+##################################################
+""")
+
+results = run_learning(environment=FromScratchShrink,
+                       learning_id="CONTINUE-SHRINK",
+                       continuous_learning=True,
+                       stop_on_max_episodes=False,
+                       parallel_environments=4,
+                       time_steps=int(20e6)
+                       )
+
+print(f"""
+################# Learning End ########################
+Results: {results}
+#######################################################
+""")
+
+print("""######### Learning from scratch #########
+R(t) = 25 - 20te - 100Bo + 20P - 18we
+Target = [0 0 1 0 0 np.pi/2]
 ##################################################
 """)
 
 results = run_learning(environment=FromScratch,
-                       learning_id="FROM-SCRATCH-10M-EPISODES",
-                       parallel_environments=4,
-                       time_steps=int(10e8),
-                       episodes=int(10e6)
+                       learning_id="FROM_SCRATCH_PARALLEL_10_20M_TS",
+                       stop_on_max_episodes=False,
+                       parallel_environments=10,
+                       time_steps=int(20e6)
                        )
 
 print(f"""
