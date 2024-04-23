9ff431b77d5bd69141d85cc112fe99c432f1fe6e
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-04-23 12:29:21 +0200

    Set a new training process using the continuous environment with 10 parallel environments and 15 million time-steps

--- gym_pybullet_drones/experiments/execute_secuencial_learning.py
+++ gym_pybullet_drones/experiments/execute_secuencial_learning.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-from gym_pybullet_drones.envs import FromScratch
+from gym_pybullet_drones.envs import Continuous
 from gym_pybullet_drones.experiments.learning_script import run_learning
 
-print("""######### Learning from scratch starting from random position #########
+print("""######### Continuous Learning #########
 R(t) = 25 - 20te - 100Bo + 20P - 18we
 Target = [0 0 1 0 0 1.5]
 ##################################################
 """)
 
-results = run_learning(environment=FromScratch,
-                       learning_id="FROM-SCRATCH-USING-SQUARED-ROOT-ROLL2+PITCH2",
-                       continuous_learning=False,
+results = run_learning(environment=Continuous,
+                       learning_id="CONTINUOUS_SHRINK_EXPLORATION_FROM_04042024220716",
+                       continuous_learning=True,
                        stop_on_max_episodes=False,
                        parallel_environments=10,
-                       time_steps=int(20e6)
+                       time_steps=int(15e6)
                        )
 
 print(f"""
