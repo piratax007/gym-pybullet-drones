d8c560d1ddcaec0dc4e00c493a4276df56a206b0
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-05-14 14:37:13 +0200

    Added three new from scratch learning corresponding to the paper

--- gym_pybullet_drones/experiments/execute_secuencial_learning.py
+++ gym_pybullet_drones/experiments/execute_secuencial_learning.py
@@ -1,19 +1,59 @@
 #!/usr/bin/env python3
-from gym_pybullet_drones.envs import ActionsFilter
+from gym_pybullet_drones.envs import ObservationSpace12, ObservationSpace72, ObservationSpace72Filter
 from gym_pybullet_drones.experiments.learning_script import run_learning
 
-print("""######### ActionsFilter Learning #########
+print("""######### From scratch learning observation space 12 #########
 R(t) = 25 - 20te - 100Bo + 20P - 18we
-Target = [0 0 1 0 0 1.5]
+Target = [0 0 1 0 0 random]
 ##################################################
 """)
 
-results = run_learning(environment=ActionsFilter,
-                       learning_id="NEW_TRAINING_72_OBSERVATION_SPACE_ACTIONS_FILTER",
+results = run_learning(environment=ObservationSpace12,
+                       learning_id="OBS-SIZE-12_FULL-REWARD_TARGET-[0 0 1 0 0 rand]",
                        continuous_learning=False,
                        stop_on_max_episodes=False,
                        parallel_environments=4,
-                       time_steps=int(10e3)
+                       time_steps=int(15e6)
+                       )
+
+print(f"""
+################# Learning End ########################
+Results: {results}
+#######################################################
+""")
+
+print("""######### From scratch learning observation space 72 #########
+R(t) = 25 - 20te - 100Bo + 20P - 18we
+Target = [0 0 1 0 0 random]
+##################################################
+""")
+
+results = run_learning(environment=ObservationSpace72,
+                       learning_id="OBS-SIZE-72_FULL-REWARD_TARGET-[0 0 1 0 0 rand]",
+                       continuous_learning=False,
+                       stop_on_max_episodes=False,
+                       parallel_environments=4,
+                       time_steps=int(15e6)
+                       )
+
+print(f"""
+################# Learning End ########################
+Results: {results}
+#######################################################
+""")
+
+print("""######### From scratch learning observation space 72 with filter #########
+R(t) = 25 - 20te - 100Bo + 20P - 18we
+Target = [0 0 1 0 0 random]
+##################################################
+""")
+
+results = run_learning(environment=ObservationSpace72Filter,
+                       learning_id="OBS-SIZE-72-FILTER_FULL-REWARD_TARGET-[0 0 1 0 0 rand]",
+                       continuous_learning=False,
+                       stop_on_max_episodes=False,
+                       parallel_environments=4,
+                       time_steps=int(15e6)
                        )
 
 print(f"""
