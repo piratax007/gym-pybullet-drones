185cea82d900462d9c6030effaa58838218c19fd
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-05-29 18:50:02 +0200

    Set to train the first task in curriculum learning by 20 million time step

--- gym_pybullet_drones/experiments/execute_secuencial_learning.py
+++ gym_pybullet_drones/experiments/execute_secuencial_learning.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-from gym_pybullet_drones.envs import ObS12Stage3
+from gym_pybullet_drones.envs import ObS12Stage1
 from gym_pybullet_drones.experiments.learning_script import run_learning
 
-print("""############# From scratch STAGE 3 #############
-Starting = [rand rand rand 0 0 0]
-Target = [0 0 1 0 0 rand]
+print("""############# From scratch STAGE 1 #############
+Starting = [0 0 0 0 0 0]
+Target = [0 0 1 0 0 0]
 ##################################################
 """)
 
-results = run_learning(environment=ObS12Stage3,
-                       learning_id="FROM-SCRATCH_STAGE-3_HOVER-STARTING-RANDOM-POSITION_RANDOM-TARGET-YAW",
-                       continuous_learning=True,
+results = run_learning(environment=ObS12Stage1,
+                       learning_id="CL-TASK1",
+                       continuous_learning=False,
                        stop_on_max_episodes=False,
                        parallel_environments=4,
                        time_steps=int(20e6)
