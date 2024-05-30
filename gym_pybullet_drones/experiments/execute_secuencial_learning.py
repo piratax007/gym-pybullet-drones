067daf3dcb97cd839c845906972be8d6c5d0d852
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-05-30 15:53:43 +0200

    Improved typos

--- gym_pybullet_drones/experiments/execute_secuencial_learning.py
+++ gym_pybullet_drones/experiments/execute_secuencial_learning.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from gym_pybullet_drones.envs import ObS12Stage1
+from gym_pybullet_drones.envs import ObS12Stage2
 from gym_pybullet_drones.experiments.learning_script import run_learning
 
 print("""############# From scratch STAGE 2 #############
@@ -8,7 +8,7 @@ Target = [0 0 1 0 0 0]
 ##################################################
 """)
 
-results = run_learning(environment=ObS12Stage1,
+results = run_learning(environment=ObS12Stage2,
                        learning_id="CL-TASK2",
                        continuous_learning=True,
                        stop_on_max_episodes=False,
