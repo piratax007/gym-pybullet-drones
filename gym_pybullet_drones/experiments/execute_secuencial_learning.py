b2baad9106c789a6e0b41301fab5987d4f10d639
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-06-01 09:37:37 +0200

    Deleted starting attitude

--- gym_pybullet_drones/experiments/execute_secuencial_learning.py
+++ gym_pybullet_drones/experiments/execute_secuencial_learning.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-from gym_pybullet_drones.envs import ObS12Rw5
+from gym_pybullet_drones.envs import ObS12Stage3
 from gym_pybullet_drones.experiments.learning_script import run_learning
 
 print("""############# From scratch STAGE 2 #############
-Starting = [rand rand rand rand rand rand]
+Starting = [rand rand rand Nan Nan Nan]
 Target = [0 0 1 0 0 rand]
 ##################################################
 """)
 
-results = run_learning(environment=ObS12Rw5,
-                       learning_id="CL-FROM-SCRATCH",
-                       continuous_learning=False,
+results = run_learning(environment=ObS12Stage3,
+                       learning_id="CL-new_TASK3",
+                       continuous_learning=True,
                        stop_on_max_episodes=False,
                        parallel_environments=4,
-                       time_steps=int(30e6)
+                       time_steps=int(16e6)
                        )
 
 print(f"""
