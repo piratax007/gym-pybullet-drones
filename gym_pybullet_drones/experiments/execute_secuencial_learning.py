6e3f02df354367886a478585e7214a3227d8ad3d
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-09-07 14:45:03 +0200

    Base training 2: initial position: [0 0 0] initial attitude: [0 0 0] pybullet frequency: 240 control frequency: 30 batch size: 128 parallel environments: 4

--- gym_pybullet_drones/experiments/execute_secuencial_learning.py
+++ gym_pybullet_drones/experiments/execute_secuencial_learning.py
@@ -3,13 +3,13 @@ from gym_pybullet_drones.envs import ObS12Stage1
 from gym_pybullet_drones.experiments.learning_script import run_learning
 
 print("""############# Base training #############
-Starting = [0 0 0 Nan Nan Nan]
+Starting = [0 0 0 0 0 0]
 Target = [0 0 1 Nan Nan Nan]
 ##################################################
 """)
 
 results = run_learning(environment=ObS12Stage1,
-                       learning_id="HOVER_BASE_TRAINING_1",
+                       learning_id="HOVER_BASE_TRAINING_2",
                        continuous_learning=False,
                        stop_on_max_episodes=False,
                        parallel_environments=4,
