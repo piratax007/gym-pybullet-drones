d731b3916223318ea9a9059b80ab6fde47464163
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-09-10 09:29:36 +0200

    Base training 7: drone mass: 0.027 kg initial position: [0 0 0] initial attitude: [0 0 0] pybullet frequency: 240 control frequency: 30 batch size: 128 learning rate: 1e-4 n_steps: 2048 -> 4096 n_epochs: 10 -> 8 clip_range: 0.2 -> 0.15 parallel environments: 4 device: auto Truncate conditions Exploration cylinder radius: 5 cm Target z distance: 5 cm Roll and pitch: 14°

--- gym_pybullet_drones/experiments/learning_script.py
+++ gym_pybullet_drones/experiments/learning_script.py
@@ -34,10 +34,10 @@ def get_ppo_model(environment, path, reuse_model=False):
                environment,
                tensorboard_log=path + '/tb/',
                batch_size=128,
-               learning_rate=1.5e-4,
+               learning_rate=1e-4,
                n_steps=4096,
                n_epochs=8,
-               clip_range=0.18,
+               clip_range=0.15,
                verbose=0,
                device='auto')
 
