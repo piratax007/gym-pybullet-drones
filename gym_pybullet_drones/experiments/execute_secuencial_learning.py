d70e33199f23c296df62d8e2a61c39679afb5587
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-09-12 16:31:45 +0200

    Base training 17: ENVIRONMENT Reward structure r = 0.25 - 0.20T - 1E + 0.20S - 0.18w Truncate conditions Exploration cylinder radius: 5 cm Target z distance: 5 cm Roll and pitch: 14° LEARNING drone mass: 0.033 kg initial position: [0 0 0] initial attitude: [0 0 0] pybullet frequency: 400 control frequency: 200 episode length: 5 s batch size: 128 learning rate: 1.5e-4 n_steps: 4096 n_epochs: 12 clip_range: 0.15 parallel environments: 4 time_steps: 80 M device: auto

--- gym_pybullet_drones/experiments/execute_secuencial_learning.py
+++ gym_pybullet_drones/experiments/execute_secuencial_learning.py
@@ -10,7 +10,7 @@ Target = [0 0 1 Nan Nan Nan]
 
 results = run_learning(environment=HoverCrazyflieSim2Real,
                        learning_id="HOVER_BASE_TRAINING-17",
-                       continuous_learning=True,
+                       continuous_learning=False,
                        stop_on_max_episodes=False,
                        parallel_environments=4,
                        time_steps=int(80e6)
