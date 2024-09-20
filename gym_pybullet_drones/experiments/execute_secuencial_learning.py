7700c263dcdce471e7a3c706eac07989f315ba7f
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-09-20 15:40:06 +0200

    WCL-DISTURBANCES-RANDOM-VELOCITIES_1 drone mass: 0.027 kg initial position: [0 0 0] initial attitude: [0 0 0] pybullet frequency: 240 control frequency: 30 batch size: 256 learning rate: 2e-4 n_steps: 8192 n_epochs: 6 clip_range: 0.12 ent_coef: 0.001 parallel environments: 4 device: auto Ground effect Wind

--- gym_pybullet_drones/experiments/execute_secuencial_learning.py
+++ gym_pybullet_drones/experiments/execute_secuencial_learning.py
@@ -10,7 +10,7 @@ results = run_learning(environment=ObS12Stage3,
                        parallel_environments=4,
                        time_steps=int(100e6),
                        stop_on_max_episodes=dict(stop=False, episodes=0),
-                       stop_on_reward_threshold=dict(stop=False, threshold=600.),
+                       stop_on_reward_threshold=dict(stop=True, threshold=600.),
                        save_checkpoints=dict(save=True, save_frequency=25000)
                        )
 
--- gym_pybullet_drones/experiments/learning_script.py
+++ gym_pybullet_drones/experiments/learning_script.py
@@ -34,11 +34,12 @@ def get_ppo_model(environment, path, reuse_model=False):
     return PPO('MlpPolicy',
                environment,
                tensorboard_log=path + '/tb/',
-               batch_size=128,
-               learning_rate=1.5e4,
-               n_steps=5376,
-               n_epochs=8,
+               batch_size=256,
+               learning_rate=2e-4,
+               n_steps=8192,
                clip_range=0.12,
+               n_epochs=6,
+               ent_coef=0.001,
                verbose=0,
                device='auto')
 
